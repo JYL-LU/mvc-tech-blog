2c445c18a903c30516f2cefa0a61722575b80068
Jane Lu <jyl.janelu@gmail.com>
2021-10-20 15:47:12 -0700

    db server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
